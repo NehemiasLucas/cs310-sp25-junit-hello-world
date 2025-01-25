d4f357a26795866f00e55ba92a6870d50940c363
Nehemias <nloarcalucas@stu.jsu.edu>
2025-01-25 11:49:16 -0600

    Initial Commit Assignment 2

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -6,18 +6,63 @@ public class Main {
         
         Main m = new Main();
         String message = m.getGreeting();
+        String message1 = m.getMessage1();
+        String message2 = m.getMessage2();
+        String message3 = m.getMessage3();
+        String message4 = m.getMessage4();
+
         
-        System.out.println(message);
+        //Displays       
+        System.out.println(message);// Greeting
         System.out.println(m.reverse(message));
         
+        System.out.println(message1);// Message1
+        System.out.println(m.reverse(message1));
+        
+        System.out.println(message2);// Message2
+        System.out.println(m.reverse(message2)); 
+        
+        System.out.println(message3);// Message3
+        System.out.println(m.reverse(message3)); 
+        
+        System.out.println(message4);// Message4
+        System.out.println(m.upperCase(message4)); 
+        
+        
+        
     }
-    
+    // Getters
     public String getGreeting() {
         return "Hello, World!";
     }
+    public String getMessage1(){
+    return "I'm a Little Teapot";
+    }
+    public String getMessage2(){
+    return "Hey Diddle Diddle";
+    }
+    public String getMessage3(){
+    return "Hickory Dickory Dock";
+    }
+    public String getMessage4(){
+    return "Hello spring at jsu";
+    }
     
+    
+    
+    //Reversals method
     public String reverse(String message) {
-        return message;
+        StringBuilder r = new StringBuilder(message);// converts String to StringBuilder
+        r.reverse(); //reverse StringBuilder
+        String newmessage = new String(r); // Changes results backs into String and stores into new var
+        
+        return newmessage; // Returns new var
+    }
+    //Uppercase method
+    public String upperCase(String message){
+        String u = message.toUpperCase();
+        
+        return u;
     }
     
 }
--- test/edu/jsu/mcis/cs310/ExampleTest.java
+++ test/edu/jsu/mcis/cs310/ExampleTest.java
@@ -7,7 +7,7 @@ public class ExampleTest {
 
     private Main main;
     private String expectedGreeting, expectedReverseGreeting;
-    private String expectedReverse1, expectedReverse2, expectedReverse3;
+    private String expectedReverse1, expectedReverse2, expectedReverse3,expectedUpper4;
     
     @Before
     public void setUp() {
@@ -17,6 +17,7 @@ public class ExampleTest {
         expectedReverse1 = "topaeT elttiL a m'I";
         expectedReverse2 = "elddiD elddiD yeH";
         expectedReverse3 = "kcoD yrokciD yrokciH";
+        expectedUpper4 = "HELLO SPRING AT JSU"; 
     }
         
     @Test
@@ -47,6 +48,11 @@ public class ExampleTest {
     public void testReverseMessage3() {
         String actual = main.reverse("Hickory Dickory Dock");
         assertEquals(expectedReverse3, actual);
+    }
+    @Test //by nehemias
+    public void testUpperMessage4(){
+        String actual = main.upperCase("Hello spring at jsu");
+        assertEquals(expectedUpper4, actual);
     }
     
 }
